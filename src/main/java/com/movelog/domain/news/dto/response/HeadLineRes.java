c44ee800bbb9a03af8f570cee40464ad89a3f27b
EunbeenDev <ros4312@naver.com>
2025-01-12 17:35:13 +0900

    [FEAT] 사용자 기록 내용 기반 헤드라인 생성 기능 구현

---
+++ src/main/java/com/movelog/domain/news/dto/response/HeadLineRes.java
@@ -0,0 +1,16 @@
+package com.movelog.domain.news.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class HeadLineRes {
+    @Schema( type = "String", example ="5년 만의 첫 도전, 무엇이 그를 움직이게 했나?", description="뉴스 헤드라인 추천 내용입니다.")
+    private String headLine;
+}
