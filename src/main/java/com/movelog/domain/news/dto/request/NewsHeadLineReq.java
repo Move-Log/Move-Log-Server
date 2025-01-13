100f54018a67f8ec4254f4833ead4910262824e7
EunbeenDev <ros4312@naver.com>
2025-01-13 23:06:33 +0900

    [FIX] 헤드라인 생성 시 키워드Id를 파라미터로 받음으로써 요청값 수정

--- src/main/java/com/movelog/domain/news/dto/request/NewsHeadLineReq.java
+++ src/main/java/com/movelog/domain/news/dto/request/NewsHeadLineReq.java
@@ -13,12 +13,4 @@ import lombok.NoArgsConstructor;
 public class NewsHeadLineReq {
     @Schema( type = "String", example ="첫 도전, 오랜만에 다시, 꾸준히 이어온 기록, 끊어낸 습관 중 택 1", description="뉴스 헤드라인 고정 옵션입니다.")
     private String option;
-
-    @Schema( type = "String", example ="했어요, 먹었어요, 갔어요 중 택 1", description="사용자가 선택한 동사 정보입니다.")
-    private String verb;
-
-    @Schema( type = "String", example ="클라이밍", description="사용자가 선택한 명사 정보입니다.")
-    private String noun;
-
-
 }
