2583fe372f261d7808029b9c716289e6299d8dcf
EunbeenDev <ros4312@naver.com>
2025-02-06 16:09:01 +0900

    [FIX] 키워드 문자열 기준 검색 결과 그룹화 및 통계 결과 그룹화 수정 반영

--- src/main/java/com/movelog/domain/record/domain/repository/KeywordRepository.java
+++ src/main/java/com/movelog/domain/record/domain/repository/KeywordRepository.java
@@ -6,9 +6,12 @@ import com.movelog.domain.record.domain.VerbType;
 import com.movelog.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface KeywordRepository extends JpaRepository<Keyword,Long> {
@@ -22,8 +25,32 @@ public interface KeywordRepository extends JpaRepository<Keyword,Long> {
 
     List<Keyword> findAllByUserAndKeywordContaining(User user, String keyword);
 
-    @Query("SELECT k FROM Keyword k WHERE LOWER(k.keyword) LIKE LOWER(CONCAT(:keyword, '%'))")
-    List<Keyword> findAllKeywordStartingWith(String keyword);
 
+    // 사용자가 기록한 것 중 동일한 keyword(명사)를 가진 Keyword 리스트 조회
+    @Query("SELECT k FROM Keyword k WHERE k.user.id = :userId AND k.keyword = :keyword")
+    List<Keyword> findByUserIdAndKeyword(@Param("userId") Long userId, @Param("keyword") String keyword);
+
+    // 특정 키워드 ID 목록에 해당하는 전체 기록 개수 조회
+    @Query("SELECT COUNT(r) FROM Record r WHERE r.keyword.keywordId IN :keywordIds")
+    int countByKeywordIds(@Param("keywordIds") List<Long> keywordIds);
+
+    // 특정 키워드 ID 목록에 해당하는 가장 최근 기록 시간 조회
+    @Query("SELECT MAX(r.actionTime) FROM Record r WHERE r.keyword.keywordId IN :keywordIds")
+    Optional<LocalDateTime> findLastRecordedAtByKeywordIds(@Param("keywordIds") List<Long> keywordIds);
+
+    // 특정 키워드 ID 목록에 해당하는 일일 평균 기록 수 계산
+    @Query("SELECT COUNT(r), DATE(r.actionTime) FROM Record r WHERE r.keyword.keywordId IN :keywordIds GROUP BY DATE(r.actionTime)")
+    List<Object[]> calculateAvgDailyRecordsByKeywordIds(@Param("keywordIds") List<Long> keywordIds);
+
+    // 특정 키워드 ID 목록에 해당하는 최근 7일간 평균 기록 수 계산
+    @Query("SELECT COUNT(r) FROM Record r WHERE r.keyword.keywordId IN :keywordIds AND r.actionTime >= :startDate")
+    long calculateAvgWeeklyRecordsByKeywordIds(@Param("keywordIds") List<Long> keywordIds, @Param("startDate") LocalDateTime startDate);
+
+    @Query("SELECT MIN(k.keywordId) AS keywordId, k.keyword " +
+            "FROM Keyword k " +
+            "WHERE LOWER(k.keyword) LIKE LOWER(CONCAT(:keyword, '%')) " +
+            "GROUP BY k.keyword " +
+            "ORDER BY MIN(k.keywordId) ASC")
+    List<Object[]> findAllKeywordStartingWith(@Param("keyword") String keyword);
 
 }
