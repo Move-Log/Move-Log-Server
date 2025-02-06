2c9ffec5eb18f94d530b53dac8e366fa6bf55936
EunbeenDev <ros4312@naver.com>
2025-02-06 17:12:52 +0900

    [HOTFIX] 최근 생성 뉴스 목록 조회 시 정렬 기준 수정

--- src/main/java/com/movelog/domain/news/application/NewsService.java
+++ src/main/java/com/movelog/domain/news/application/NewsService.java
@@ -93,7 +93,7 @@ public class NewsService {
         int pageNumber = (page == null || page < 0) ? 0 : page;
 
         // page 적용 및 정렬 추가
-        Pageable pageable = PageRequest.of(pageNumber, 15, Sort.by(Sort.Direction.ASC, "actionTime"));
+        Pageable pageable = PageRequest.of(pageNumber, 15);
 
         // 최근 일주일간 생성한 뉴스 목록 조회
         LocalDateTime createdAt = LocalDateTime.now().minusDays(7);
